5d0a8e95a5db26094bc32b0f924c0321e95be143
Melanie Freshness <melanie.freshness@gmail.com>
2019-04-09 09:31:24 +0100

    initial commit

---
+++ Weather/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>London Weather</h1>
+    <ul>
+      <li v-for="record in data">{{ record }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    // We can use async/await ES6 feature
+    const { data } = await axios.get(`http://localhost:8000/api`)
+    return { data }
+  }
+}
+</script>
